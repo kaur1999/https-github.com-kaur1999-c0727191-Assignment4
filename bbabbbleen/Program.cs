4426712e162c7602d0612bf51c16ec2dbcfda984
visualstudio <visualstudio@122-PC26>
2019-04-05 18:45:37 -0400

    COOMIT 1

--- bbabbbleen/Program.cs
+++ bbabbbleen/Program.cs
@@ -4,13 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace bbabbbleen
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
-           int  i= 10;
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+            //Read file using StreamReader.Reads file line by line
+            using (StreamReader file = new StreamReader("c:/area51/beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+
+                }
+                file.close();
+                Console.WriteLine($"File has{counter}lines");
+            }
+        }
+        public int FindNumberofBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach(char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+            }
+            return countSpaces;
+        }
+        
+        
     }
 }
